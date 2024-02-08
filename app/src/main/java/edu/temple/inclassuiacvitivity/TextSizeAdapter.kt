ea812cd6367f45de839549d09dc7eff9355aaea7
ChungYingLee <e12123484@gmail.com>
2024-02-08 12:02:42 -0500

    create constructor for TextSizeAdapter class

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,33 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context : Context, _fontSize : Array<Int>) : BaseAdapter() {
 
+    val context = _context
+    val fontSize = _fontSize
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+//        return fontSize.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+//        return fontSize[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+//        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+//        val textView = TextView(context)
+//        textView.text = getItem(position).toString()
+//        textView.textSize = position.toFloat()
+//        textView.setPadding(5, 10, 0,10)
+//        return textView
     }
 
 }
