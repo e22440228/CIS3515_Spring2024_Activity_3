037cfe2a203f826d99cbd4ac9d279cbe312c5f46
ChungYingLee <e12123484@gmail.com>
2024-02-08 12:03:45 -0500

    finished function getCount(), getItem(), getItemId(), and getView

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -11,23 +11,23 @@ class TextSizeAdapter(_context : Context, _fontSize : Array<Int>) : BaseAdapter(
     val context = _context
     val fontSize = _fontSize
     override fun getCount(): Int {
-//        return fontSize.size
+        return fontSize.size
     }
 
     override fun getItem(position: Int): Any {
-//        return fontSize[position]
+        return fontSize[position]
     }
 
     override fun getItemId(position: Int): Long {
-//        return position.toLong()
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//        val textView = TextView(context)
-//        textView.text = getItem(position).toString()
-//        textView.textSize = position.toFloat()
-//        textView.setPadding(5, 10, 0,10)
-//        return textView
+        val textView = TextView(context)
+        textView.text = getItem(position).toString()
+        textView.textSize = position.toFloat()
+        textView.setPadding(5, 10, 0,10)
+        return textView
     }
 
 }
